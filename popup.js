e8eaf4b3a0fea9d7f75d414862b91ab845a8ad78
Brett Hevia <brett.hevia@gmail.com>
2016-12-03 15:09:30 -0700

    first commit

---
+++ popup.js
@@ -0,0 +1,30 @@
+const xhr = new XMLHttpRequest();
+let weatherKey = 'c6f9cf80abac0cc0d08971b6c53bfc3c';
+let mainText = document.querySelector('.main-text');
+let currentLocation;
+
+
+const getLocation = () => {
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      currentLocation = position.coords
+      getLocalWeather(currentLocation.latitude, currentLocation.longitude)
+    })
+  }
+}
+
+const getLocalWeather = (latitude, longitude) => {
+  fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${weatherKey}`,
+    { method: 'GET', mode: 'cors'})
+  .then((response) => {
+    console.log(response)
+  })
+  .catch(function(err) {
+});
+}
+
+
+
+
+
+getLocation();
