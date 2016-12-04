2eff5e2153491e15c871cdaec6e2e01bbf2f2cc7
Brett Hevia <brett.hevia@gmail.com>
2016-12-03 17:08:07 -0700

    remove comments

--- popup.js
+++ popup.js
@@ -2,15 +2,6 @@ const xhr = new XMLHttpRequest();
 let weatherKey = 'c6f9cf80abac0cc0d08971b6c53bfc3c';
 let mainText = document.querySelector('.main-text');
 
-let currentCity;
-
-// const unsplash = new Unsplash({
-//   applicationId: "ID 163902f30d13c54b65fef8f7605583f451ec9fd47162dc95e7f8ce2fa6fe7fb3",
-//   callbackUrl: "urn:ietf:wg:oauth:2.0:oob",
-//   bearerToken: "19f79eb91a56ded9145c15e9ab1ea225405bc13a98f1909a4910afc9c5bdc226"
-// });
-
-
 const getLocation = () => {
   if ("geolocation" in navigator) {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -37,17 +28,12 @@ const getUnsplashPicture = (weather) => {
   xhr.open("GET", `https://api.unsplash.com/photos/random?query=${weather}&w=800&h=800&client_id=163902f30d13c54b65fef8f7605583f451ec9fd47162dc95e7f8ce2fa6fe7fb3`, true)
   xhr.onreadystatechange = () => {
     if (xhr.readyState == 4) {
-      console.log(JSON.parse(xhr.response))
-      let image = JSON.parse(xhr.response).urls.full
-      mainText.style.backgroundImage = `url(${image})`
+      let response = JSON.parse(xhr.response)
+      response.urls.full ? mainText.style.backgroundImage = `url(${response.urls.full})` : null
     }
+    if (xhr.readyState !== 4) { console.log('too many calls, son')}
   }
   xhr.send()
 }
-
-// https://api.unsplash.com/photos/random
-// urn:ietf:wg:oauth:2.0:oob
-// ?client_id=YOUR_APPLICATION_ID
-
 
 getLocation();
